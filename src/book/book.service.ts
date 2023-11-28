b43d49c715eb9778508a19791af1abf3bfb67607
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-11-28 01:37:13 -0300

    Fix validation for book ID

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Book } from './schemas/book.schema';
 import * as mongoose from 'mongoose';
@@ -21,6 +25,12 @@ export class BookService {
   }
 
   async findById(id: string): Promise<Book> {
+    const isValidId = mongoose.isValidObjectId(id);
+
+    if (!isValidId) {
+      throw new BadRequestException('Please provide a valid id');
+    }
+
     const book = await this.bookModel.findById(id);
 
     if (!book) {
@@ -31,6 +41,12 @@ export class BookService {
   }
 
   async updateById(id: string, book: Book): Promise<Book> {
+    const isValidId = mongoose.isValidObjectId(id);
+
+    if (!isValidId) {
+      throw new BadRequestException('Please provide a valid id');
+    }
+
     const updateBook = await this.bookModel.findByIdAndUpdate(id, book, {
       new: true,
       runValidators: true,
